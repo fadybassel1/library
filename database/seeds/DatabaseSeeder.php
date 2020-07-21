fedf88b052b8ea68fa6fad2edc4c5fd40a421966
Mark <mark.refaat.ramzy@gmail.com>
2020-07-21 23:33:03 +0200

    role seeds
    dataentry, moderator, admin, superadmin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,15 +17,52 @@ class DatabaseSeeder extends Seeder
       array(
         'name' => "fady",
         'username' => 'fady',
+        'role' => 'superadmin',
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
       ),
       array(
         'name' => "mark",
         'username' => 'mark',
+        'role' => 'superadmin',
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
       ),
+
+      array(
+        'name' => "data entry",
+        'username' => 'de',
+        'role' => 'dataentry',
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+      ),
+
+      array(
+        'name' => "moderator",
+        'username' => 'moderator',
+        'role' => "moderator",
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+      ),
+
+      array(
+        'name' => "admin",
+        'username' => 'admin',
+        'role' => "admin",
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+      ),
+
+      array(
+        'name' => "super admin",
+        'username' => 'sadmin',
+        'role' => "superadmin",
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+      ),
+
+
+
     ));
   }
 }
